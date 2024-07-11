30585cba5dc0a22174aa459eac8f4555446c4522
Sayand2002 <sayandak2002@gmail.com>
2024-07-11 13:38:18 +0530

    Completed front end and added few more features like random match and wallet UI.

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { UserRoutes } from './routes/userRoutes/UserRoutes';
 import { AdminRoutes } from './routes/adminRoutes/AdminRoutes';
 import "./App.css"
-// import { SocketProvider } from './context/socketContext';
+import { SocketProvider } from './context/socketContext';
 
 
 export function App() {
     return (
-        // <SocketProvider>
+        <SocketProvider>
             <Router>
                 <Routes>
                     <Route path='/*' element={<UserRoutes />} />
                     <Route path='/admin/*' element={<AdminRoutes />} />
                 </Routes>
             </Router>
-        // </SocketProvider>
+        </SocketProvider>
     );
 }
 
