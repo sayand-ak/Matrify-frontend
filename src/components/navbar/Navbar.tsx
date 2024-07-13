42d3e4e596961b9fa2b8d68fe06b9aabc863700e
Sayand2002 <sayandak2002@gmail.com>
2024-07-13 10:56:59 +0530

    added notified users list in navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -300,7 +300,7 @@ export default function Navbar({page}: NavbarProps){
 
                                 {totalNotificationsCount > 0 ? (
                                     <div 
-                                        className="bounce2 absolute -top-1 right-1 bg-red-500 rounded-full text-white text-sm w-5 h-5 flex items-center justify-center"
+                                        className="bounce2 absolute top-0 right-0 bg-red-500 rounded-full text-white text-sm w-5 h-5 flex items-center justify-center"
                                         onMouseOver={() => setIsNotificationDropdownVisible(!isNotificationDropdownVisible)}
                                     >
                                         {totalNotificationsCount}
