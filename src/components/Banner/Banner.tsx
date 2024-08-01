d32c7b250181257c8159b7e916a8dfde16bc8404
Sayand2002 <sayandak2002@gmail.com>
2024-08-01 16:58:37 +0530

    Changed landing UI

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -10,14 +10,17 @@ export function Banner() {
       className="banner max-w-full flex pt-[10rem] md:pt-0 items-start md:items-center px-16 text-[25px] lg:text-[50px] text-[#000000]"
     >
       <motion.h1
-        className="font-semibold w-1/2 bg-red-400"
+        className="font-semibold w-1/2"
         variants={textVariants}
         initial="hidden"
         animate="visible"
       >
         Discover the path to Love and Unity with Matrify - Where souls connect in Harmony.
       </motion.h1>
-      <div className="images w-1/2 rounded-b-[60px] md:rounded-l-[50px] md:rounded-none"></div>
+      
+      <div className="images w-full md:w-1/2 rounded-b-[60px] md:rounded-l-[50px] md:rounded-none">
+        {/* Add background image or other styles here */}
+      </div>
     </div>
   );
 }
